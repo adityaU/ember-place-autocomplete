79a25ae875c7fc4503deb69d0c5596a0ca39b277
Aditya Upadhyay <adi@iitj.ac.in>
2016-04-23 13:56:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ module.exports = {
   name: 'ember-place-autocomplete',
   contentFor: function(type, config) {
     var content = '';
-    if (type === 'body-footer') {
-      var src = "//maps.googleapis.com/maps/api/js",
-         placeAutocompleteConfig = config['place-autocomplete'] || {},
-         params = [],
-         key = placeAutocompleteConfig.key;
-      if (key)
-        params.push('key=' + encodeURIComponent(key));
-      src += '?' + params.join('&') + "&libraries=places";
-      content = '<script type="text/javascript" src="' + src + '"></script>';
-    }
     return content;
   }
 };
