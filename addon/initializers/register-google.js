459a16e0cb195a3fa8fc08663f9243828c883f21
Aditya Upadhyay <adi@iitj.ac.in>
2016-04-23 15:22:51 +0530

    Update register-google.js

--- addon/initializers/register-google.js
+++ addon/initializers/register-google.js
@@ -1,8 +1,8 @@
 export function initialize() {
   let application = arguments[1] || arguments[0];
 
-  application.register('google:main', window.google, { instantiate: false });
-  application.inject('component', 'google', 'google:main');
+  //application.register('google:main', window.google, { instantiate: false });
+  //application.inject('component', 'google', 'google:main');
 }
 
 export default {
